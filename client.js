bcbf926982af7cd49fcf611b3bcca93e8eecc04d
Jessica Nelson <jessica.nelson60@gmail.com>
2023-10-07 08:51:36 -0600

    displayed and modified chosen number and string on client.js

---
+++ client.js
@@ -0,0 +1,33 @@
+let myNumber = 5467; // MyNumber variable set to 5467
+console.log(typeof myNumber); // Checking data type of variable
+
+console.log(myNumber); // Expected output 5467
+
+console.log(myNumber.toExponential()); // Expected output, exponential of "a"
+
+console.log(Number.isInteger(myNumber)); //Expected output, is "a" an integer? (true or false)
+
+console.log(Number.isNaN(myNumber)); // Expected output, is "a" a NaN? (true or false)
+
+console.log(Number.isFinite(myNumber)); // Expected output, is "a" finite? (true or false)
+
+
+const myName = ("Jessica" + " " + "Michelle"); // myName variable set to Jessica Michelle
+console.log(typeof myName); // Checking data type of variable
+
+const firstCharacter = myName[0]; // Get the first character of "myName"
+
+console.log(myName); // Expected output, Jessica Michelle
+
+console.log(myName.length); // Expected output, length of string
+
+console.log(firstCharacter); // Expected output, first character of string
+
+console.log(myName.replace("Michelle", "Nelson")); // Expected output, Jessica Nelson
+
+console.log(myName.startsWith("Jess")); // Expected output, does string start with "Jess"? (true of false) 
+
+console.log(myName.endsWith("cool")); // Expected output, does string end with "cool"? (true or false)
+
+
+
